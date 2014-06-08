f2b4baad53d252a559978f2ce6d5aedd2b8c8055
Robert Whitney <xnite@xnite.org>
2014-06-08 08:16:50 -0500

    added keepchans which will auto update channel db

---
+++ modules/keepchans.php
@@ -0,0 +1,16 @@
+<?php
+$irc->registerModule(
+	"keepchans",
+	"xnite <xnite@xnite.org>",
+	array(),
+	array()
+);
+$irc->hook_join(keepchans_join);
+
+function keepchans_join($x = array()) {
+	global $irc; global $db; global $me;
+	if($x['nick'] == $me) {
+		$db->channel_add($x['chan']);
+		echo '[INFO] Adding '.$x['chan']."to database so we will auto-join it next time\n";
+	}
+}
