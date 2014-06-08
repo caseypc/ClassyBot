05c18b27431d98174b319fee72a8c72951d127e4
Robert Whitney <xnite@xnite.org>
2014-06-08 05:45:26 -0500

    greet in channel when others join
    Greet was happening in PM with user joining, this was accidental and has
    now been fixed.

--- modules/greet.php
+++ modules/greet.php
@@ -10,7 +10,7 @@ $irc->hook_join(greet_others_join);
 function greet_others_join($x = array()) {
 	global $irc; global $me;
 	if($x['nick'] != $me) {
-		$irc->privmsg($x['nick'], "Hello ".$x['nick'].", welcome to ".$x['channel']."!");
+		$irc->privmsg($x['chan'], "Hello ".$x['nick'].", welcome to ".$x['channel']."!");
 	} else {
 		$irc->privmsg($x['chan'], "Hello everybody! I am ".$me.", and some might say I'm rather classy!");
 	}
